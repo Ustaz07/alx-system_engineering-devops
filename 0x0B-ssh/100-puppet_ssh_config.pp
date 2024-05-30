c15449afdae87219392f6a7708a98960d0518d4d
Ustaz07 <shamsutukor@gmail.com>
2024-05-30 07:29:35 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,24 +1,19 @@
-# 100-puppet_ssh_config.pp
-
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
+# Ensure SSh client configuration
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
 }
 
-augeas { 'Set IdentityFile':
-  context => '/files/etc/ssh/ssh_config',
-  changes => [
-    'set Host/*/IdentityFile ~/.ssh/school',
-  ],
-  onlyif  => 'match Host/*/IdentityFile[.="~/.ssh/school"] size == 0',
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
 }
 
-augeas { 'Turn off PasswordAuthentication':
-  context => '/files/etc/ssh/ssh_config',
-  changes => [
-    'set Host/*/PasswordAuthentication no',
-  ],
-  onlyif  => 'match Host/*/PasswordAuthentication[.="no"] size == 0',
+# Reload SSH service if configuration changes
+service { 'ssh':
+  ensure  => running,
+  enable  => true,
+  require => File_line['Turn off passwd auth', 'Declare identity file'],
 }
