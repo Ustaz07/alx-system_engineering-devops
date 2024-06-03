c88a4f795356df7814d9e85ee9f069718e12c658
Ustaz07 <shamsutukor@gmail.com>
2024-06-03 11:56:05 -0700

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,8 +12,7 @@ def make_json(users, todos):
     """Turns payloads into JSON format"""
     user_id = users[0]['id']
     username = users[0]['username']
-    tasks = [{"task": todo['title'], "completed": todo['completed'],
-              "username": username} for todo in todos]
+    tasks = [{"task": todo['title'], "completed": todo['completed'], "username": username} for todo in todos]
 
     with open(f"{user_id}.json", "w") as f:
         json.dump({user_id: tasks}, f)
@@ -25,9 +24,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    users = requests.get(
-        "https://jsonplaceholder.typicode.com/users", params={"id": employee_id}).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos",
-                         params={"userId": employee_id}).json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users", params={"id": employee_id}).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": employee_id}).json()
 
     make_json(users, todos)
+
