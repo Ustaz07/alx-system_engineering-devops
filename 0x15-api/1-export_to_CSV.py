772ed4e0d5b9b7f31563170c83aa81e2ec6f3b85
Ustaz07 <shamsutukor@gmail.com>
2024-06-03 11:32:15 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,47 +1,37 @@
+#!/usr/bin/python3
+"""
+Script that uses REST API to fetch user's tasks and export them to CSV format.
+"""
+
 import csv
 import requests
 import sys
 
-def fetch_user_and_todos(employee_id):
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
 
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Unable to fetch data")
-        sys.exit(1)
+def make_csv(users, todos):
+    """Turns payloads into CSV format"""
+    titles = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
-    user = user_response.json()
-    todos = todos_response.json()
+    with open(f"{users[0]['id']}.csv", "w", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=titles, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+        for todo in todos:
+            writer.writerow({
+                "USER_ID": todo["userId"],
+                "USERNAME": users[0]["username"],
+                "TASK_COMPLETED_STATUS": todo["completed"],
+                "TASK_TITLE": todo["title"]
+            })
 
-    return user, todos
-
-def validate_csv(user, todos):
-    user_id = user["id"]
-    user_name = user["username"]
-    expected_file_name = f"{user_id}.csv"
-    expected_num_tasks = len(todos)
-
-    # Check if CSV file exists
-    try:
-        with open(expected_file_name, newline='') as file:
-            reader = csv.reader(file)
-            header = next(reader)
-            actual_num_tasks = sum(1 for row in reader)
-            return header == ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"] and \
-                   actual_num_tasks == expected_num_tasks
-    except FileNotFoundError:
-        return False
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: ./1-export_to_CSV.py <employee_id>")
+        print("Usage: ./script.py <employee_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    user, todos = fetch_user_and_todos(employee_id)
-    valid_csv = validate_csv(user, todos)
-    if not valid_csv:
-        print("CSV file validation failed")
-    else:
-        print("CSV file validation passed")
+    employee_id = sys.argv[1]
+    users = requests.get("https://jsonplaceholder.typicode.com/users", params={"id": employee_id}).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": employee_id}).json()
+
+    make_csv(users, todos)
 
